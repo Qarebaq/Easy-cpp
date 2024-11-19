14fd9a8a04c7bd3a346df4593343f4bdea2cac12
Amir Mohammad Mohammadi <95334969+Qarebaq@users.noreply.github.com>
2024-11-19 21:26:49 +0330

    Add files via upload

---
+++ square.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+void triangle(int n){
+for (int i = 0; i < n;i++){
+for (int i = 0 ; i < n ; i++){
+
+    cout<<"*";
+
+}
+cout<<endl;
+}
+}
+int main(){
+int n;
+cin>>n;
+triangle(n);
+    
+}
